244e29259c8199b156d650dc128419ac33105240
Haene Safavi <hsafavi@madisoncollege.edu>
2023-10-08 17:53:12 -0500

    Week 6 Activity - Exception Handling

--- week6/ec2.py
+++ week6/ec2.py
@@ -1,24 +1,7 @@
-#!/usr/bin/env python3
 import boto3
-import json
-import botocore.exceptions
+import botocore
 
-
-s3client = boto3.client('s3')
-
-bucket_list = s3client.list_buckets()
-
-for bucket in bucket_list['Buckets']:
-    bucket_name = bucket['Name']
-
-    objects = s3client.list_objects_v2(Bucket=bucket_name)
-    if 'Contents' in objects:
-        for obj in objects['Contents']:
-            object_key = obj['Key']
-            s3client.delete_object(Bucket=bucket_name, Key=object_key)
-            print(f"Deleted object: {object_key} in bucket: {bucket_name}")
-    s3client.delete_bucket(Bucket=bucket_name)
-    print(f"Deleted bucket: {bucket_name}")
+client = boto3.client('ec2')
 
 def Get_Image(ec2client):
     image_response = ec2client.describe_images(
@@ -39,42 +22,56 @@ def Get_Image(ec2client):
     )
     return image_response['Images'][0]['ImageId']
 
-def Create_EC2(AMI, ec2client):
-
+def Create_EC2(ec2client, AMI):
     DRYRUN = False
 
     try:
         response = ec2client.run_instances(
-            ImageId=AMI,
-            InstanceType='t2.micro',
-            MaxCount=1,
-            MinCount=1,
-            DryRun=DRYRUN
+        ImageId=AMI,
+        InstanceType='t2.micro',
+        #SecurityGroupIds=['sg-0635021150d9779d6'],
+        UserData='''#!/bin/bash -ex
+        # Updated to use Amazon Linux 2
+        yum -y update
+        yum -y install httpd php mysql php-mysql
+        /usr/bin/systemctl enable httpd
+        /usr/bin/systemctl start httpd
+        cd /var/www/html
+        wget https://aws-tc-largeobjects.s3-us-west-2.amazonaws.com/CUR-TF-100-ACCLFO2/lab6-scaling/lab-app.zip
+        unzip lab-app.zip -d /var/www/html/
+        chown apache:root /var/www/html/rds.conf.php
+        ''',
+        MaxCount=1,
+        MinCount=1,
+        DryRun=DRYRUN
         )
         return response['Instances'][0]['InstanceId']
     except botocore.exceptions.ClientError as error:
         if error.response['Error']['Code'] == 'UnauthorizedOperation':
-            print("UnauthorizedOperation: You may not have permissions to create EC2 instances in this region.")
-            print("Please check your AWS configuration, specifically the region in ~/.aws/config.")
-        elif "InvalidAMIID.NotFound" in str(error):
-            print("InvalidAMIID: The specified AMI ID does not exist or is invalid.")
-        elif "InvalidInstanceID.NotFound" in str(error):
-            print("InvalidInstanceID: The specified instance ID does not exist.")
+            print("You do not have authorization to create EC2 instances in this region.\nPlease check your AWS config.")
+        elif error.response['Error']['Code'] == 'InvalidParameterValue':
+            print("InvalidParameterValue: The provided parameters are invalid. Please check the parameters.")
         else:
-            print(f"An error occurred while creating EC2 instance: {error}")
+            print(f"The following error has occurred: {error}")
+
 
 def main():
-    client = boto3.client('ec2')
-    AMI = Get_Image(client)
+    try:
+        client = boto3.client('ec2')
+        AMI = Get_Image(client)
+        instance_id = Create_EC2(client, AMI)
 
-    instance_id = Create_EC2(AMI, client)
+        ec2 = boto3.resource('ec2')
+        instance = ec2.Instance(instance_id)
+        print(f"Before Waiting: Instance is {instance.state['Name']}")
+        instance.wait_until_running()
+        instance.load()
+        print(f"After Waiting: Instance is {instance.state['Name']}")
+    except botocore.exceptions.ClientError as error:
+        if error.response['Error']['Code'] == 'AuthFailure':
+            print("You are not authorized to perform the action, please check that your credentials are correct and you have permission to perform the action.")
+        else:
+            print(f"Some other error has occurred: {error}")
 
-    ec2 = boto3.resource('ec2')
-    instance = ec2.Instance(instance_id)
-    print(f"Before Waiting: Instance is {instance.state['Name']}")
-    instance.wait_until_running()
-    instance.load()
-    print(f"After Waiting: Instance is {instance.state['Name']}")
-    
 if __name__ == "__main__":
     main()
--- week6/s3website.py
+++ week6/s3website.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
-
 import boto3,json,datetime,random,argparse,string
 
 import botocore
 
-s3client = boto3.client('s3')
+client = boto3.client('s3')
 
 bucket_name = "hsafavi-fall2023-buckettest"
 
@@ -20,7 +18,7 @@ else:
     print(f"You specified a bucket name of {args.site_name}.")
     bucket_name = args.site_name
 try:
-    bucket_response = s3client.create_bucket(Bucket=bucket_name)
+    bucket_response = client.create_bucket(Bucket=bucket_name)
 
     bucket_policy = {
         'Version': '2012-10-17',
@@ -35,14 +33,14 @@ try:
 
     bucket_policy_string = json.dumps(bucket_policy)
 
-    s3client.delete_public_access_block(Bucket=bucket_name)
+    client.delete_public_access_block(Bucket=bucket_name)
 
-    bucket_policy_response = s3client.put_bucket_policy(
+    bucket_policy_response = client.put_bucket_policy(
             Bucket=bucket_name,
             Policy=bucket_policy_string
     )
 
-    put_bucket_response = s3client.put_bucket_website(
+    put_bucket_response = client.put_bucket_website(
         Bucket=bucket_name, 
         WebsiteConfiguration={ 
             'ErrorDocument': {'Key': 'error.html'}, 
@@ -51,21 +49,23 @@ try:
     )
 
     indexFile = open('index.html', 'rb')
-    put_index_response = s3client.put_object(Body=indexFile, Bucket=bucket_name, Key='index.html',ContentType='text/html')
+    put_index_response = client.put_object(Body=indexFile, Bucket=bucket_name, Key='index.html',ContentType='text/html')
     indexFile.close()
     print(put_index_response)
 
     errorFile = open('error.html', 'rb')
-    put_error_response = s3client.put_object(Body=errorFile, Bucket=bucket_name, Key='error.html', ContentType='text/html')
+    put_error_response = client.put_object(Body=errorFile, Bucket=bucket_name, Key='error.html', ContentType='text/html')
     errorFile.close()
     print(put_error_response)
 
 except botocore.exceptions.ClientError as error:
     if error.response['Error']['Code'] == 'InvalidToken':
-        print("Please update your AWS credentials with a valid token")
-    else:
-        print(f"Some other error occurred: {error}")
+        print("Please update your AWS credentials file with a valid token")
+    elif error.response['Error']['Code'] == 'IllegalLocationConstraintException':
+        print("The location selected is incompatible for the region specific endpoint the request was sent to.")
 
-except s3client.exceptions.BucketAlreadyExists as err:
-    print(f"Bucket '{err.response['Error']['BucketName']}' already exists!")
-    print("Re-run the script with a valid bucket name")
+except client.meta.client.exceptions.BucketAlreadyExists as err:
+        print("Bucket {} already exists!".format(err.response['Error']['BucketName']))
+        print("Re-run the script with a valid bucket name")
+else:
+        print(f"Some other error occurred: {error}")
